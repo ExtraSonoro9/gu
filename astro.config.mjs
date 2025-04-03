1c734e4c72daa4c385bf6a2f26404b0a3ac75023
ExtraSonoro9 <tomasdomingoxxi@gmai.com>
2025-04-03 11:12:45 -0300

    magenes 	modified:   astro.config.mjs 	new file:   src/assets/images.png 	new file:   src/assets/logo.svg 	modified:   src/content/docs/guides/example.md 	modified:   src/content/docs/index.mdx 	deleted:    src/content/docs/reference/example.md 	modified:   gu (modified content)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,22 +6,20 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
+			title: 'Guía Universal',
+			logo: {
+				src: './src/assets/logo.svg',
+			  },
+			
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Prueba', slug: 'guides/example' },
 					],
 				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+		
 			],
 		}),
 	],
