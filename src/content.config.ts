4eb6863991ef1773b27df394030add47ba5db6ec
ExtraSonoro9 <tomasdomingoxxi@gmail.com>
2025-08-06 19:27:05 +0000

    doo

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,8 +3,6 @@ import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 
-
-
 export const collections = {
 	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
 };
